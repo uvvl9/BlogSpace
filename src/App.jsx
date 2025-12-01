9557e197e37ef08b30d005966b594958e9fd88d5
Abdullah Alahmadi <aalahmadi10010@gmail.com>
2025-12-01 07:04:54 +0300

    fix routing and logo for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router>
+        <Router basename="/BlogSpace">
             <ThemeProvider>
                 <AuthProvider>
                     <div className="app">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
             <div className="container">
                 <div className="header-content">
                     <Link to="/" className="logo">
-                        <img src="/logo.png" alt="BlogSpace" style={{ height: '40px', width: 'auto' }} />
+                        <img src={`${import.meta.env.BASE_URL}logo.png`} alt="BlogSpace" style={{ height: '40px', width: 'auto' }} />
                         <span className="logo-text text-gradient">BlogSpace</span>
                     </Link>
 
